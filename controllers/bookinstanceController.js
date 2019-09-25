aa5f07fde3adbd1d48933f96f149aad85d43baea
aspiringguru <bmatthewtaylor@gmail.com>
2019-09-25 13:00:32 +1000

    chapter 5-10 BookInstance detail page - also broken

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -15,7 +15,25 @@ exports.bookinstance_list = function(req, res) {
 
 // Display detail page for a specific BookInstance.
 exports.bookinstance_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id);
+    //res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id);
+    console.log("exports.bookinstance_detail start");
+    BookInstance.findById(req.params.id)
+    .populate('book')
+    .exec(function (err, bookinstance) {
+      if (err) {
+        console.log("exports.bookinstance_detail err:"+err);
+        return next(err);
+      }
+      if (bookinstance==null) { // No results.
+          console.log("exports.bookinstance_detail Book copy not found.");
+          var err = new Error('Book copy not found');
+          err.status = 404;
+          return next(err);
+        }
+      // Successful, so render.
+      console.log("exports.bookinstance_detail success, rendering.");
+      res.render('bookinstance_detail', { title: 'Copy: '+bookinstance.book.title, bookinstance:  bookinstance});
+    })
 };
 
 // Display BookInstance create form on GET.
